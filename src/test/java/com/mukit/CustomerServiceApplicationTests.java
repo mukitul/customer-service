b3718163e81f76682bc69a0bbabd42a27708cc18
mukitul <mukitulislamratul@gmail.com>
2020-01-19 01:50:51 +0600

    first commit

---
+++ src/test/java/com/mukit/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
