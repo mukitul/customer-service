e3c68cdf71b49c49e920076a57feb8ad26e5a8c0
mukitul <mukitulislamratul@gmail.com>
2020-01-20 14:51:12 +0600

    working on best practice

--- src/main/java/com/mukit/controller/implementation/CustomerMasterImplementation.java
+++ src/main/java/com/mukit/controller/implementation/CustomerMasterImplementation.java
@@ -1,7 +1,17 @@
 package com.mukit.controller.implementation;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.mukit.controller.interfaces.CustomerMaster;
 
+@RestController
+@RequestMapping("customermaster")
 public class CustomerMasterImplementation implements CustomerMaster {
-
+	
+	
+	
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
 }
