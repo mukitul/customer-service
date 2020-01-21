f0aea778c53391481e097e45b520ead5d3c0622f
mukitul <mukitulislamratul@gmail.com>
2020-01-21 12:16:05 +0600

    data and view class added

--- src/main/java/com/mukit/model/data/CustomerMasterData.java
+++ src/main/java/com/mukit/model/data/CustomerMasterData.java
@@ -1,5 +1,62 @@
 package com.mukit.model.data;
 
-public class CustomerMasterData {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "customer_master")
+public class CustomerMasterData {
+	
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	
+	
+	@NotNull
+	@Column(name = "customer_id")
+	private Integer customerId;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+
+	public CustomerMasterData() {
+		
+	}
+	
+	public CustomerMasterData(Integer id, @NotNull Integer customerId) {
+		super();
+		this.id = id;
+		this.customerId = customerId;
+	}
+
+
+	@Override
+	public String toString() {
+		return "CustomerMasterData [id=" + id + ", customerId=" + customerId + "]";
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/mukit/model/view/CustomerMasterView.java
+++ src/main/java/com/mukit/model/view/CustomerMasterView.java
@@ -1,5 +1,47 @@
 package com.mukit.model.view;
 
 public class CustomerMasterView {
+	private Integer id;
+	private Integer customerId;
+	
+	
+	public Integer getId() {
+		return id;
+	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+
+
+
+	public CustomerMasterView() {
+		
+	}
+
+	public CustomerMasterView(Integer id, Integer customerId) {
+		super();
+		this.id = id;
+		this.customerId = customerId;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerMasterView [id=" + id + ", customerId=" + customerId + "]";
+	}
+	
+	
+	
+	
 }
